b6986fe450221ecf0a0d419ef7e932ec29990576
krzysztoffila <umowy.fila@gmail.com>
2024-03-04 13:54:33 +0100

    Cleaning structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
 <template>
   <div id="app" data-theme="cupcake">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-    
   </div>
 </template>
 <style lang="scss"></style>
