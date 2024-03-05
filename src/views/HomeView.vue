c656aa15ce82be70a67f5c39b29aaaf1b493f416
krzysztoffila <umowy.fila@gmail.com>
2024-03-05 13:41:10 +0100

    Add glass effect in hero-content text

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hero min-h-screen bg-cover bg-center" style="background-image: url('https://images.unsplash.com/photo-1576091160399-112ba8d25d1d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');">
     <div class="hero-content flex-col lg:flex-row-reverse">
-      <div class="text-center lg:text-left">
+      <div class="text-center lg:text-left glass p-5 rounded text-white">
         <h1 class="text-5xl font-bold">Login now!</h1>
         <p class="py-6">Join us and experience the future of pet healthcare registration. Sign up for quick and convenient access to online veterinary appointments. We're here to provide you with a seamless registration process and expert care for your furry companions.</p>
       </div>
