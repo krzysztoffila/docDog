3140365d3d1c42eb85110012fac5cc2965de2335
krzysztoffila <umowy.fila@gmail.com>
2024-03-05 12:29:46 +0100

    Added Navbar Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <div id="app" data-theme="cupcake">
+  <div id="app" data-theme="aqua">
+    <header>
+      <NavbarComponent/>
+    </header>
   </div>
 </template>
+<script>
+import NavbarComponent from "@/components/navbar-component.vue"
+export default {
+  components:{
+    NavbarComponent
+  }
+}
+</script>
 <style lang="scss"></style>
 
