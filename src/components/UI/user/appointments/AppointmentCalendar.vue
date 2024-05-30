d6a9b3f660cb31ae98925ead06d399c1698ca2a2
krzysztoffila <umowy.fila@gmail.com>
2024-05-30 14:23:36 +0200

    Add datepicker

---
+++ src/components/UI/user/appointments/AppointmentCalendar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <date-picker v-model="selectedDate" valueType="format"></date-picker>
+  </div>
+</template>
+
+<script>
+import DatePicker from "vue2-datepicker";
+import "vue2-datepicker/index.css";
+
+export default {
+  components: { DatePicker },
+  data() {
+    return {
+      selectedDate: null,
+    };
+  },
+};
+</script>
