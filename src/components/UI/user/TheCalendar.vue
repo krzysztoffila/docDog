2bf1d34f067c6f9bae24d14ea20f4f15e71b0849
krzysztoffila <umowy.fila@gmail.com>
2024-06-05 08:50:51 +0200

    Add vue calendar

--- src/components/UI/user/TheCalendar.vue
+++ src/components/UI/user/TheCalendar.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <date-picker v-model="selectedDate" valueType="format"></date-picker>
-  </div>
+  <vue-cal
+    class="vuecal--rounded-theme vuecal--blue-theme"
+    xsmall
+    hide-view-selector
+    :time="false"
+    active-view="month"
+    :disable-views="['week']"
+    style="width: 270px; height: 300px"
+  >
+  </vue-cal>
 </template>
 
 <script>
-import DatePicker from "vue2-datepicker";
-import "vue2-datepicker/index.css";
+import VueCal from "vue-cal";
+import "vue-cal/dist/vuecal.css";
 
 export default {
-  components: { DatePicker },
+  components: { VueCal },
   data() {
     return {
       selectedDate: null,
