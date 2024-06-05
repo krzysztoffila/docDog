1f661c92769db8d4d980487814e238269f59b738
krzysztoffila <umowy.fila@gmail.com>
2024-06-05 14:07:58 +0200

    Fixed appointments props

--- src/components/UI/user/appointments/AppointmentList.vue
+++ src/components/UI/user/appointments/AppointmentList.vue
@@ -3,26 +3,26 @@
     <h2 class="text-lg font-semibold mb-4">Your Appointments:</h2>
     <ul>
       <li
-        v-for="(date, index) in appointments"
+        v-for="(appointment, index) in appointments"
         :key="index"
         class="flex items-center mb-2"
       >
         <BadgeElement>
           <template v-if="index === 0" #pending>
-            {{ date.date }}
-            <div class="badge-info badge mr-4">{{ date.status }}</div>
+            <div class="badge-info badge mr-4">{{ appointment.status }}</div>
+            {{ appointment.date }}
           </template>
           <template v-else-if="index === 1" #visited>
-            {{ date.date }}
-            <div class="badge-success badge mr-4">{{ date.status }}</div>
+            <div class="badge-success badge mr-4">{{ appointment.status }}</div>
+            {{ appointment.date }}
           </template>
           <template v-else-if="index === 2" #cancelled>
-            {{ date.date }}
-            <div class="badge-error badge mr-4">{{ date.status }}</div>
+            <div class="badge-error badge mr-4">{{ appointment.status }}</div>
+            {{ appointment.date }}
           </template>
           <template v-else #pending>
-            {{ date.date }}
-            <div class="badge-info badge mr-4">{{ date.status }}</div>
+            <div class="badge-info badge mr-4">{{ appointment.status }}</div>
+            {{ appointment.date }}
           </template>
         </BadgeElement>
       </li>
@@ -37,7 +37,10 @@ export default {
     BadgeElement,
   },
   props: {
-    appointments: Array,
+    appointments: {
+      type: Array,
+      required: true,
+    },
   },
 };
 </script>
