40c091b73c0c049e9e07d4aa56b5757412faf549
krzysztoffila <umowy.fila@gmail.com>
2024-06-07 11:17:12 +0200

    Add v-model on doctors

--- src/components/UI/doctor/DoctorsComponent.vue
+++ src/components/UI/doctor/DoctorsComponent.vue
@@ -3,23 +3,50 @@
     <label for="doctors" class="mb-2 font-bold">Choose a doctor:</label>
     <div id="doctors" class="flex flex-col space-y-2">
       <label class="flex items-center">
-        <input type="radio" name="doctor" value="dolittle" class="mr-2" />
+        <input
+          type="radio"
+          name="doctor"
+          value="dolittle"
+          v-model="selectedDoctor"
+          class="mr-2"
+        />
         Dr. Dolittle
       </label>
       <label class="flex items-center">
-        <input type="radio" name="doctor" value="vetura" class="mr-2" />
+        <input
+          type="radio"
+          name="doctor"
+          value="vetura"
+          v-model="selectedDoctor"
+          class="mr-2"
+        />
         Ace Ventura
       </label>
       <label class="flex items-center">
-        <input type="radio" name="doctor" value="oetker" class="mr-2" />
+        <input
+          type="radio"
+          name="doctor"
+          value="oetker"
+          v-model="selectedDoctor"
+          class="mr-2"
+        />
         Dr. Oetker
       </label>
+    </div>
+    <div class="mt-4">
+      <p>Selected doctor: {{ selectedDoctor }}</p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      selectedDoctor: null,
+    };
+  },
+};
 </script>
 
 <style>
