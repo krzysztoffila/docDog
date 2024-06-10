464b4fd5731f375720bba4c293212edccf318b77
krzysztoffila <umowy.fila@gmail.com>
2024-06-10 13:46:59 +0200

    Add user obj to vuex

--- src/store/index.js
+++ src/store/index.js
@@ -6,31 +6,33 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     userIsLogged: true,
-    email: "j.store@mail.com",
-    fullName: "John Store",
-    appointmentsData: [
-      {
-        date: "27.08.2024",
-        status: "Pending",
-        doctor: "Dr.Dolittle",
-      },
-      {
-        date: "28.09.2024",
-        status: "Visited",
-        doctor: "Dr.Ventura",
-      },
-      {
-        date: "29.10.2024",
-        status: "Cancelled",
-        doctor: "Dr.Oetker",
-      },
-      {
-        date: "30.11.2024",
-        status: "Pending",
-        doctor: "Dr.Oetker",
-      },
-    ],
     selectedDoctor: null,
+    user: {
+      email: "j.store@mail.com",
+      fullName: "John Store",
+      appointmentsData: [
+        {
+          date: "27.08.2024",
+          status: "Pending",
+          doctor: "Dr.Dolittle",
+        },
+        {
+          date: "28.09.2024",
+          status: "Visited",
+          doctor: "Dr.Ventura",
+        },
+        {
+          date: "29.10.2024",
+          status: "Cancelled",
+          doctor: "Dr.Oetker",
+        },
+        {
+          date: "30.11.2024",
+          status: "Pending",
+          doctor: "Dr.Oetker",
+        },
+      ],
+    },
     doctors: [
       {
         doctorId: 0,
