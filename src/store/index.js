47ec8c0cb6b67ed107a1e4b6739d020beeb6fe57
krzysztoffila <umowy.fila@gmail.com>
2024-06-24 17:10:46 +0200

    Register Toast module

--- src/store/index.js
+++ src/store/index.js
@@ -1,62 +1,63 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import Toast from "./modules/toast-module";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    userIsLogged: true,
-    selectedDoctor: null,
-    user: {
-      email: "j.store@mail.com",
-      fullName: "John Store",
-      appointmentsData: [
-        {
-          date: "27.08.2024",
-          status: "Pending",
-          doctor: "Dr.Dolittle",
-        },
-        {
-          date: "28.09.2024",
-          status: "Visited",
-          doctor: "Dr.Ventura",
-        },
-        {
-          date: "29.10.2024",
-          status: "Cancelled",
-          doctor: "Dr.Oetker",
-        },
-        {
-          date: "30.11.2024",
-          status: "Pending",
-          doctor: "Dr.Oetker",
-        },
-      ],
+    modules: {
+        Toast,
     },
-    doctors: [
-      {
-        doctorId: 0,
-        name: "Dr. Dolittle",
-      },
-      {
-        doctorId: 1,
-        name: "Dr. Ventura",
-      },
-      {
-        doctorId: 2,
-        name: "Dr. Oetker",
-      },
-    ],
-  },
-  getters: {
-  },
-  mutations: {
-    setSelectedDoctor(state, doctor) {
-      state.selectedDoctor = doctor;
-    }
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        userIsLogged: true,
+        selectedDoctor: null,
+        user: {
+            email: "j.store@mail.com",
+            fullName: "John Store",
+            appointmentsData: [
+                {
+                    date: "27.08.2024",
+                    status: "Pending",
+                    doctor: "Dr.Dolittle",
+                },
+                {
+                    date: "28.09.2024",
+                    status: "Visited",
+                    doctor: "Dr.Ventura",
+                },
+                {
+                    date: "29.10.2024",
+                    status: "Cancelled",
+                    doctor: "Dr.Oetker",
+                },
+                {
+                    date: "30.11.2024",
+                    status: "Pending",
+                    doctor: "Dr.Oetker",
+                },
+            ],
+        },
+        doctors: [
+            {
+                doctorId: 0,
+                name: "Dr. Dolittle",
+            },
+            {
+                doctorId: 1,
+                name: "Dr. Ventura",
+            },
+            {
+                doctorId: 2,
+                name: "Dr. Oetker",
+            },
+        ],
+    },
+    getters: {},
+    mutations: {
+        setSelectedDoctor(state, doctor) {
+            state.selectedDoctor = doctor;
+        },
+    },
+    actions: {},
+    modules: {},
+});
