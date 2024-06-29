e4fdfa8b7f241408a15adcf07d500e3db0009a4a
krzysztoffila <umowy.fila@gmail.com>
2024-06-29 21:03:50 +0200

    Add BaseModal

---
+++ src/components/layout/BaseModal.vue
@@ -0,0 +1,56 @@
+<template>
+  <dialog ref="modal" class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">{{ title }}</h3>
+      <p class="py-4">{{ message }}</p>
+      <div class="modal-action">
+        <button class="btn" @click="confirmAction">{{ confirmText }}</button>
+        <button class="btn" @click="closeModal">{{ closeText }}</button>
+      </div>
+    </div>
+  </dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "Modal Title",
+    },
+    message: {
+      type: String,
+      default: "Modal Message",
+    },
+    confirmText: {
+      type: String,
+      default: "Confirm",
+    },
+    closeText: {
+      type: String,
+      default: "Close",
+    },
+    onConfirm: {
+      type: Function,
+      default: null,
+    },
+  },
+  methods: {
+    openModal() {
+      this.$refs.modal.showModal();
+    },
+    closeModal() {
+      this.$refs.modal.close();
+    },
+    confirmAction() {
+      if (this.onConfirm) {
+        this.onConfirm();
+      }
+      this.closeModal();
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
