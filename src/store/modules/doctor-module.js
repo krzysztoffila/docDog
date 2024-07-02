2d291bbcf6cfc76a6fe26d1aeede3099d0fdc730
krzysztoffila <umowy.fila@gmail.com>
2024-07-02 11:01:04 +0200

    add actions to doctor module

--- src/store/modules/doctor-module.js
+++ src/store/modules/doctor-module.js
@@ -2,23 +2,20 @@ export default {
     namespaced: true,
     state: {
         doctors: [
-            {
-                doctorId: 0,
-                name: "Dr. Dolittle",
-            },
-            {
-                doctorId: 1,
-                name: "Dr. Ventura",
-            },
-            {
-                doctorId: 2,
-                name: "Dr. Oetker",
-            },
+            { doctorId: 0, name: "Dr. Dolittle" },
+            { doctorId: 1, name: "Dr. Ventura" },
+            { doctorId: 2, name: "Dr. Oetker" },
         ],
+        selectedDoctor: null,
     },
     mutations: {
         setSelectedDoctor(state, doctor) {
             state.selectedDoctor = doctor;
+        },
+    },
+    actions: {
+        setSelectedDoctor({ commit }, doctor) {
+            commit("setSelectedDoctor", doctor);
         },
     },
 };
