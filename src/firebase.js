51ba1543303a9c8e6d6c720e0e47df1747f28c19
krzysztoffila <umowy.fila@gmail.com>
2024-07-24 15:55:09 +0200

    Docs: delete firebase from gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+// .env
 const firebaseConfig = {
     apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
     authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
