c691d668d3a0924ecbf05d8a1d64693e17907da8
krzysztoffila <umowy.fila@gmail.com>
2024-07-30 11:31:37 +0200

    Feature: Add login form in heading

--- src/components/UI/home/HeadingLogin.vue
+++ src/components/UI/home/HeadingLogin.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-    <form @submit.prevent="loginUser" class="card-body">
+    <form @submit.prevent="submitFormLogin" class="card-body">
       <div class="form-control">
         <label class="label">
           <span class="label-text">Email</span>
@@ -41,6 +41,8 @@
 
 <script>
 import BaseButton from "@/components/layout/BaseButton.vue";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Cookies from "js-cookie";
 export default {
   components: { BaseButton },
   data() {
@@ -50,13 +52,34 @@ export default {
     };
   },
   methods: {
-    loginUser() {
-      const userData = {
-        email: this.email,
-        password: this.password,
-      };
-      const { email, password } = userData;
-      alert(`Data: ${email}, password: ${password}`);
+    async submitFormLogin() {
+      const auth = getAuth();
+      try {
+        const userCredential = await signInWithEmailAndPassword(
+          auth,
+          this.email,
+          this.password
+        );
+        const user = userCredential.user;
+
+        const idToken = await user.getIdToken();
+        sessionStorage.setItem("userToken", idToken);
+        Cookies.set("userToken", idToken, { expires: 1 });
+
+        this.$store.commit("user/setUser", user);
+        this.$store.commit("Toast/addToast", {
+          message: `Hello, ${this.email}!`,
+          variant: "alert-success",
+        });
+        this.$router.push("/dashboard");
+      } catch (error) {
+        this.$store.commit("Toast/addToast", {
+          message: "Login failed. Please check your credentials.",
+          variant: "alert-danger",
+        });
+      }
+      this.email = "";
+      this.password = "";
     },
   },
 };
